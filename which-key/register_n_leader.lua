99ed18479daaa4bf700d9ec4c8fd95b37a273a24
Micah Halter <micah@balena.io>
2022-03-28 11:28:38 -0400

    Clean up capitalization
    Signed-off-by: Micah Halter <micah@balena.io>

--- which-key/register_n_leader.lua
+++ which-key/register_n_leader.lua
@@ -293,12 +293,12 @@ local Nmappings = {
 }
 
 local Dmappings = {
-  n = { "<C-r>=strftime('%Y-%m-%d')<cr>", "Y-m-d" },
-  x = { "<C-r>=strftime('%m/%d/%y')<cr>", "m/d/y" },
-  f = { "<C-r>=strftime('%B %d, %Y')<cr>", "B d, Y" },
-  X = { "<C-r>=strftime('%H:%M')<cr>", "H:M" },
-  F = { "<C-r>=strftime('%H:%M:%S')<cr>", "H:M:S" },
-  d = { "<C-r>=strftime('%Y/%m/%d %H:%M:%S -')<cr>", "Y/m/d H:M:S -" },
+  n = { "<c-r>=strftime('%Y-%m-%d')<cr>", "Y-m-d" },
+  x = { "<c-r>=strftime('%m/%d/%y')<cr>", "m/d/y" },
+  f = { "<c-r>=strftime('%B %d, %Y')<cr>", "B d, Y" },
+  X = { "<c-r>=strftime('%H:%M')<cr>", "H:M" },
+  F = { "<c-r>=strftime('%H:%M:%S')<cr>", "H:M:S" },
+  d = { "<c-r>=strftime('%Y/%m/%d %H:%M:%S -')<cr>", "Y/m/d H:M:S -" },
 }
 
 local Vmappings = {
