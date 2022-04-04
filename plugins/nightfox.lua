90728cb3402f88983903bfd16670dbbf8c05c5d0
Micah Halter <micah@balena.io>
2022-04-04 13:35:09 -0400

    speed up nightfox

--- plugins/nightfox.lua
+++ plugins/nightfox.lua
@@ -7,6 +7,22 @@ return {
     inverse = {
       match_paren = true,
     },
+    modules = {
+      barbar = false,
+      dashboard = false,
+      fern = false,
+      fidget = false,
+      gitgutter = false,
+      illuminate = false,
+      lightspeed = false,
+      lsp_saga = false,
+      lsp_trouble = false,
+      modes = false,
+      neogit = false,
+      nvimtree = false,
+      pounce = false,
+      sneak = false,
+    },
   },
   groups = {
     MiniIndentscopeSymbol = { link = "PreProc" },
