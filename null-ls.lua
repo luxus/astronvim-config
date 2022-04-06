f0def79a5e9283c2ef3767ccb2e2b0b484291b62
Micah Halter <micah@balena.io>
2022-04-06 08:31:53 -0400

    Clean up null-ls
    Signed-off-by: Micah Halter <micah@balena.io>

--- null-ls.lua
+++ null-ls.lua
@@ -1,40 +1,32 @@
 return function()
   local status_ok, null_ls = pcall(require, "null-ls")
-  if not status_ok then
-    return
+  if status_ok then
+    local builtins = null_ls.builtins
+
+    null_ls.setup {
+      debug = false,
+      sources = {
+        builtins.code_actions.gitsigns,
+        builtins.code_actions.shellcheck,
+        builtins.formatting.stylua,
+        builtins.formatting.black,
+        builtins.formatting.format_r,
+        builtins.formatting.isort,
+        builtins.formatting.prettier,
+        builtins.formatting.shfmt,
+        builtins.diagnostics.cue_fmt,
+        builtins.diagnostics.shellcheck,
+      },
+      on_attach = function(client)
+        vim.notify(client.name, "info", { title = "Language Server", timeout = 500 })
+        if client.resolved_capabilities.document_formatting then
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            desc = "Auto format before save",
+            pattern = "<buffer>",
+            callback = vim.lsp.buf.formatting_sync,
+          })
+        end
+      end,
+    }
   end
-
-  local code_actions = null_ls.builtins.code_actions
-  local formatting = null_ls.builtins.formatting
-  local diagnostics = null_ls.builtins.diagnostics
-
-  null_ls.setup {
-    debug = false,
-    sources = {
-      -- Set code actions
-      code_actions.gitsigns,
-      code_actions.shellcheck,
-      -- Set a formatter
-      formatting.stylua,
-      formatting.black,
-      formatting.format_r,
-      formatting.isort,
-      formatting.prettier,
-      formatting.shfmt,
-      -- Set a linter
-      diagnostics.cue_fmt,
-      diagnostics.shellcheck,
-    },
-    -- NOTE: You can remove this on attach function to disable format on save
-    on_attach = function(client)
-      vim.notify(client.name, "info", { title = "Language Server", timeout = 500 })
-      if client.resolved_capabilities.document_formatting then
-        vim.api.nvim_create_autocmd("BufWritePre", {
-          desc = "Auto format before save",
-          pattern = "<buffer>",
-          callback = vim.lsp.buf.formatting_sync,
-        })
-      end
-    end,
-  }
 end
