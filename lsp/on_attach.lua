12b4d36fb5b34cfdc13f454f1988ccbf0b62dd33
Micah Halter <micah@balena.io>
2022-04-28 16:52:11 -0400

    Update on_attach
    Signed-off-by: Micah Halter <micah@balena.io>

--- lsp/on_attach.lua
+++ lsp/on_attach.lua
@@ -1,7 +1,5 @@
 return function(client, bufnr)
   if client.name == "sqls" then
     require("sqls").on_attach(client, bufnr)
-  elseif client.name == "r_language_server" and vim.o.filetype == "rmd" then
-    client.resolved_capabilities.document_formatting = false
   end
 end
