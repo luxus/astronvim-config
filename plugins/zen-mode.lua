407ea6b71ce1a40b33ac66951c5e8c8473e79632
Micah Halter <micah@balena.io>
2022-04-28 09:59:09 -0400

    Clean up zen-mode config
    Signed-off-by: Micah Halter <micah@balena.io>

--- plugins/zen-mode.lua
+++ plugins/zen-mode.lua
@@ -1,14 +1,8 @@
 return {
   window = {
     backdrop = 0.95, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
-    -- height and width can be:
-    -- * an absolute number of cells when > 1
-    -- * a percentage of the width / height of the editor when <= 1
-    -- * a function that returns the width or the height
     width = 120, -- width of the Zen window
     height = 1, -- height of the Zen window
-    -- by default, no options are changed for the Zen window
-    -- uncomment any of the options below, or add other vim.wo options you want to apply
     options = {
       -- signcolumn = "no", -- disable signcolumn
       number = false, -- disable number column
@@ -20,8 +14,6 @@ return {
     },
   },
   plugins = {
-    -- disable some global vim options (vim.o...)
-    -- comment the lines to not apply the options
     options = {
       enabled = true,
       ruler = false, -- disables the ruler text in the cmd line area
@@ -30,17 +22,8 @@ return {
     twilight = { enabled = true }, -- enable to start Twilight when zen mode opens
     gitsigns = { enabled = false }, -- disables git signs
     tmux = { enabled = false }, -- disables the tmux statusline
-    -- this will change the font size on kitty when in zen mode
-    -- to make this work, you need to set the following kitty options:
-    -- - allow_remote_control socket-only
-    -- - listen_on unix:/tmp/kitty
-    kitty = {
-      enabled = false,
-      font = "+4", -- font size increment
-    },
+    kitty = { enabled = false },
   },
-  -- callback where you can add custom code when the Zen window opens
   on_open = function(win) end,
-  -- callback where you can add custom code when the Zen window closes
   on_close = function() end,
 }
