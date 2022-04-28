8d900c06b3930b9b19bfb1ad04229a1d2b85d063
Micah Halter <micah@balena.io>
2022-04-28 10:02:19 -0400

    clean up other plugin configs
    Signed-off-by: Micah Halter <micah@balena.io>

--- plugins/cmp.lua
+++ plugins/cmp.lua
@@ -1,33 +1,33 @@
 local cmp_ok, cmp = pcall(require, "cmp")
 local luasnip_ok, luasnip = pcall(require, "luasnip")
-if not cmp_ok or not luasnip_ok then
+if cmp_ok and luasnip_ok then
+  return {
+    mapping = {
+      ["<CR>"] = cmp.mapping.confirm(),
+      ["<Tab>"] = cmp.mapping(function(fallback)
+        if luasnip.expandable() then
+          luasnip.expand()
+        elseif luasnip.expand_or_jumpable() then
+          luasnip.expand_or_jump()
+        else
+          fallback()
+        end
+      end, {
+        "i",
+        "s",
+      }),
+      ["<S-Tab>"] = cmp.mapping(function(fallback)
+        if luasnip.jumpable(-1) then
+          luasnip.jump(-1)
+        else
+          fallback()
+        end
+      end, {
+        "i",
+        "s",
+      }),
+    },
+  }
+else
   return {}
 end
-
-return {
-  mapping = {
-    ["<CR>"] = cmp.mapping.confirm(),
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if luasnip.expandable() then
-        luasnip.expand()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      else
-        fallback()
-      end
-    end, {
-      "i",
-      "s",
-    }),
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, {
-      "i",
-      "s",
-    }),
-  },
-}
--- plugins/telescope.lua
+++ plugins/telescope.lua
@@ -1,54 +1,52 @@
 local status_ok, telescope = pcall(require, "telescope")
-if not status_ok then
-  return {}
-end
-
-local actions = require "telescope.actions"
-local hop = telescope.extensions.hop
-
-return {
-  defaults = {
-    selection_caret = "  ",
-    layout_config = {
-      width = 0.90,
-      height = 0.85,
-      preview_cutoff = 120,
-      horizontal = {
-        preview_width = 0.6,
-      },
-      vertical = {
-        width = 0.9,
-        height = 0.95,
-        preview_height = 0.5,
-      },
-      flex = {
+if status_ok then
+  local actions = require "telescope.actions"
+  local hop = telescope.extensions.hop
+  return {
+    defaults = {
+      selection_caret = "  ",
+      layout_config = {
+        width = 0.90,
+        height = 0.85,
+        preview_cutoff = 120,
         horizontal = {
-          preview_width = 0.9,
+          preview_width = 0.6,
+        },
+        vertical = {
+          width = 0.9,
+          height = 0.95,
+          preview_height = 0.5,
+        },
+        flex = {
+          horizontal = {
+            preview_width = 0.9,
+          },
+        },
+      },
+      mappings = {
+        i = {
+          ["<C-h>"] = hop.hop,
+          ["<C-space>"] = function(prompt_bufnr)
+            local opts = {
+              callback = actions.toggle_selection,
+              loop_callback = actions.send_selected_to_qflist,
+            }
+            hop._hop_loop(prompt_bufnr, opts)
+          end,
         },
       },
     },
-    mappings = {
-      i = {
-        -- Hop
-        ["<C-h>"] = hop.hop,
-        ["<C-space>"] = function(prompt_bufnr)
-          local opts = {
-            callback = actions.toggle_selection,
-            loop_callback = actions.send_selected_to_qflist,
-          }
-          hop._hop_loop(prompt_bufnr, opts)
-        end,
+    extensions = {
+      bibtex = {
+        context = true,
+        context_fallback = false,
+      },
+      media_files = {
+        filetypes = { "png", "jpg", "mp4", "webm", "pdf" },
+        find_cmd = "rg",
       },
     },
-  },
-  extensions = {
-    bibtex = {
-      context = true,
-      context_fallback = false,
-    },
-    media_files = {
-      filetypes = { "png", "jpg", "mp4", "webm", "pdf" },
-      find_cmd = "rg",
-    },
-  },
-}
+  }
+else
+  return {}
+end
