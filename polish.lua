17f8cf7fc711192da8ee04c0022777d0ae7e995e
Micah Halter <micah@balena.io>
2022-05-02 11:06:23 -0400

    cleanup

--- polish.lua
+++ polish.lua
@@ -1,34 +1,31 @@
 return function()
-  local set = vim.opt
-  local g = vim.g
-
   -- Options
   -- show whitespace characters
-  set.list = true
-  set.listchars = {
+  vim.opt.list = true
+  vim.opt.listchars = {
     tab = "│→",
     extends = "⟩",
     precedes = "⟨",
     trail = "·",
     nbsp = "␣",
   }
-  set.showbreak = "↪ "
+  vim.opt.showbreak = "↪ "
   -- enable conceal
-  set.conceallevel = 2
+  vim.opt.conceallevel = 2
   -- soft wrap lines
-  set.wrap = true
+  vim.opt.wrap = true
   -- linebreak soft wrap at words
-  set.linebreak = true
+  vim.opt.linebreak = true
   -- set spell and thesaurus files
-  set.spellfile = "~/.config/nvim/lua/user/spell/en.utf-8.add"
-  set.thesaurus = "~/.config/nvim/lua/user/spell/mthesaur.txt"
+  vim.opt.spellfile = "~/.config/nvim/lua/user/spell/en.utf-8.add"
+  vim.opt.thesaurus = "~/.config/nvim/lua/user/spell/mthesaur.txt"
   -- set Treesitter based folding and disable auto-folding on open
-  set.foldenable = false
-  set.foldmethod = "expr"
-  set.foldexpr = "nvim_treesitter#foldexpr()"
+  vim.opt.foldenable = false
+  vim.opt.foldmethod = "expr"
+  vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 
-  g.load_black = false
-  g.loaded_matchit = true
+  vim.g.load_black = false
+  vim.g.loaded_matchit = true
 
   -- Auto Commands
   require("user.autocmds").setup()
