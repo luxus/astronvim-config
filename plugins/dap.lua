4082c460ff074e136c3967e5715b87e1768f1c83
Micah Halter <micah@balena.io>
2022-05-03 11:27:23 -0400

    simplify dap config

--- plugins/dap.lua
+++ plugins/dap.lua
@@ -17,18 +17,20 @@ return function()
       end,
     },
   }
-  local function start_session(_, _)
-    local info_string = string.format("%s", dap.session().config.program)
-    vim.notify(info_string, "debug", { title = "Debugger Started", timeout = 500 })
+  dap.listeners.after.event_initialized["dapui"] = function(_, _)
+    vim.notify(
+      string.format("%s", dap.session().config.program),
+      "debug",
+      { title = "Debugger Started", timeout = 500 }
+    )
   end
-
-  local function terminate_session(_, _)
-    local info_string = string.format("%s", dap.session().config.program)
-    vim.notify(info_string, "debug", { title = "Debugger Terminated", timeout = 500 })
+  dap.listeners.before.event_terminated["dapui"] = function(_, _)
+    vim.notify(
+      string.format("%s", dap.session().config.program),
+      "debug",
+      { title = "Debugger Terminated", timeout = 500 }
+    )
   end
-
-  dap.listeners.after.event_initialized["dapui"] = start_session
-  dap.listeners.before.event_terminated["dapui"] = terminate_session
 
   vim.fn.sign_define("DapStopped", { text = "", texthl = "DiagnosticWarn" })
   vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "DiagnosticInfo" })
