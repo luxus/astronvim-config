b5dc32aa55698b81dce3300b45207591b530a863
Micah Halter <micah@balena.io>
2022-05-04 18:12:23 -0400

    Add custom telescope theme
    afds

--- plugins/nightfox.lua
+++ plugins/nightfox.lua
@@ -26,10 +26,29 @@ return {
       symbols_outline = false,
     },
   },
+  specs = {
+    all = {
+      telescope = {
+        bg_alt = "bg2",
+        bg = "bg1",
+        fg = "fg1",
+        green = "green",
+        red = "red",
+      },
+    },
+  },
   groups = {
-    MiniIndentscopeSymbol = { link = "PreProc" },
-    NormalFloat = { link = "Normal" },
-    TelescopeBorder = { bg = "bg1" },
     HighlightURL = { style = "underline" },
+    MiniIndentscopeSymbol = { link = "PreProc" },
+    TelescopeBorder = { fg = "telescope.bg_alt", bg = "telescope.bg" },
+    TelescopeNormal = { bg = "telescope.bg" },
+    TelescopePreviewBorder = { fg = "telescope.bg", bg = "telescope.bg" },
+    TelescopePreviewTitle = { fg = "telescope.bg", bg = "telescope.green" },
+    TelescopePromptBorder = { fg = "telescope.bg_alt", bg = "telescope.bg_alt" },
+    TelescopePromptNormal = { fg = "telescope.fg", bg = "telescope.bg_alt" },
+    TelescopePromptPrefix = { fg = "telescope.red", bg = "telescope.bg_alt" },
+    TelescopePromptTitle = { fg = "telescope.bg", bg = "telescope.red" },
+    TelescopeResultsBorder = { fg = "telescope.bg", bg = "telescope.bg" },
+    TelescopeResultsTitle = { fg = "telescope.bg", bg = "telescope.bg" },
   },
 }
