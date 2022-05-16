324d6136a11ded56fb2bde0da29e9f3adece44f2
Micah Halter <micah@balena.io>
2022-05-16 11:27:52 -0400

    Add default_theme configuration
    Signed-off-by: Micah Halter <micah@balena.io>

---
+++ default_theme/colors.lua
@@ -0,0 +1,8 @@
+return function(C)
+  C.telescope_green = C.green
+  C.telescope_red = C.red
+  C.telescope_fg = C.fg
+  C.telescope_bg = C.black_1
+  C.telescope_bg_alt = C.bg_1
+  return C
+end
---
+++ default_theme/highlights.lua
@@ -0,0 +1,14 @@
+return {
+  TelescopeBorder = { fg = C.telescope_bg_alt, bg = C.telescope_bg },
+  TelescopeNormal = { bg = C.telescope_bg },
+  TelescopePreviewBorder = { fg = C.telescope_bg, bg = C.telescope_bg },
+  TelescopePreviewNormal = { bg = C.telescope_bg },
+  TelescopePreviewTitle = { fg = C.telescope_bg, bg = C.telescope_green },
+  TelescopePromptBorder = { fg = C.telescope_bg_alt, bg = C.telescope_bg_alt },
+  TelescopePromptNormal = { fg = C.telescope_fg, bg = C.telescope_bg_alt },
+  TelescopePromptPrefix = { fg = C.telescope_red, bg = C.telescope_bg_alt },
+  TelescopePromptTitle = { fg = C.telescope_bg, bg = C.telescope_red },
+  TelescopeResultsBorder = { fg = C.telescope_bg, bg = C.telescope_bg },
+  TelescopeResultsNormal = { bg = C.telescope_bg },
+  TelescopeResultsTitle = { fg = C.telescope_bg, bg = C.telescope_bg },
+}
