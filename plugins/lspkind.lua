68454a9d875713e5c49705de396c86c7095fe63a
Micah Halter <micah@balena.io>
2022-08-19 11:13:30 -0400

    use codeicons

---
+++ plugins/lspkind.lua
@@ -0,0 +1,15 @@
+return function(config)
+  config.preset = "codicons"
+  config.symbol_map = {
+    Array = "",
+    Boolean = "",
+    Key = "",
+    Namespace = "",
+    Null = "",
+    Number = "",
+    Object = "",
+    Package = "",
+    String = "",
+  }
+  return config
+end
