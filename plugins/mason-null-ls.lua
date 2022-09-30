21ce7ecffc495e0e7926e755af6a820a0ce9587d
Micah Halter <micah@balena.io>
2022-09-30 11:09:06 -0400

    move to mason-null-ls

---
+++ plugins/mason-null-ls.lua
@@ -0,0 +1,1 @@
+return { automatic_installation = true }
--- plugins/mason-tool-installer.lua
+++ plugins/mason-tool-installer.lua
@@ -1,12 +1,6 @@
 return {
   auto_update = true,
   ensure_installed = {
-    "black",
     "debugpy",
-    "isort",
-    "prettierd",
-    "shellcheck",
-    "shfmt",
-    "stylua",
   },
 }
