e163a423601936e02d170be3582f2d039861b32d
Micah Halter <micah@balena.io>
2022-09-30 11:12:49 -0400

    null-ls provides an on attach now

--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -11,13 +11,4 @@ return {
     null_ls.builtins.diagnostics.cue_fmt,
     null_ls.builtins.diagnostics.shellcheck,
   },
-  on_attach = function(client)
-    if client.server_capabilities.documentFormattingProvider then
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        desc = "Auto format before save",
-        pattern = "<buffer>",
-        callback = function() vim.lsp.buf.format() end,
-      })
-    end
-  end,
 }
