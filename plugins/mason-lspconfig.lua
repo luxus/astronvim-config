46ee2ceec5050cbba069af8183493d88b40ee764
Micah Halter <micah@balena.io>
2022-10-06 11:06:06 -0500

    simplify language server management

--- plugins/mason-lspconfig.lua
+++ plugins/mason-lspconfig.lua
@@ -1,1 +1,18 @@
-return { automatic_installation = true }
+return {
+  automatic_installation = true,
+  ensure_installed = {
+    "clangd",
+    "cmake",
+    "cssls",
+    "html",
+    "intelephense",
+    "marksman",
+    "jsonls",
+    "pyright",
+    "sqls",
+    "sumneko_lua",
+    "texlab",
+    "tsserver",
+    "yamlls",
+  },
+}
--- plugins/mason-null-ls.lua
+++ plugins/mason-null-ls.lua
@@ -1,1 +1,14 @@
-return { automatic_installation = true }
+return {
+  automatic_installation = true,
+  ensure_installed = {
+    "gitsigns",
+    "shellcheck",
+    "stylua",
+    "black",
+    "isort",
+    "prettierd",
+    "shfmt",
+    "cue_fmt",
+    "shellcheck",
+  },
+}
