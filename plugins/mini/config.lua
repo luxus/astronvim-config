4d86e8bced0a5f48356bcbad652e4dd4f494b193
Micah Halter <micah@balena.io>
2022-10-12 13:11:06 -0400

    add more treesitter text objects

--- plugins/mini/config.lua
+++ plugins/mini/config.lua
@@ -12,7 +12,11 @@ require("mini.ai").setup {
   custom_textobjects = {
     ["*"] = gen_spec.pair("*", "*", { type = "greedy" }),
     ["_"] = gen_spec.pair("_", "_", { type = "greedy" }),
+    B = gen_spec.treesitter { a = "@block.outer", i = "@block.inner" },
+    C = gen_spec.treesitter { a = "@conditional.outer", i = "@conditional.inner" },
     F = gen_spec.treesitter { a = "@function.outer", i = "@function.inner" },
+    L = gen_spec.treesitter { a = "@loop.outer", i = "@loop.inner" },
+    P = gen_spec.treesitter { a = "@parameter.outer", i = "@parameter.inner" },
     x = gen_spec.treesitter { a = "@class.outer", i = "@class.inner" },
   },
 }
