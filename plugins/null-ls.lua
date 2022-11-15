67ed0191bb87020d0ff016dc75be82ba1ce9f1df
Micah Halter <micah@balena.io>
2022-11-15 09:27:24 -0500

    remove other null-ls sources

--- plugins/null-ls.lua
+++
@@ -1,8 +0,0 @@
-local builtins = require("null-ls").builtins
-return {
-  sources = {
-    builtins.code_actions.gitsigns,
-    builtins.diagnostics.cue_fmt,
-    builtins.formatting.cue_fmt,
-  },
-}
