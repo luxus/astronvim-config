510298d229b0fe85e0ef04f0c5b74a0d39648754
Micah Halter <micah@mehalter.com>
2022-12-14 09:41:36 -0500

    disable swapfile

--- options.lua
+++ options.lua
@@ -9,6 +9,7 @@ return {
     listchars = { tab = "│→", extends = "⟩", precedes = "⟨", trail = "·", nbsp = "␣" },
     showbreak = "↪ ",
     spellfile = vim.fn.expand "~/.config/nvim/lua/user/spell/en.utf-8.add",
+    swapfile = false,
     thesaurus = vim.fn.expand "~/.config/nvim/lua/user/spell/mthesaur.txt",
     wrap = true, -- soft wrap lines
   },
