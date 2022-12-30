b45dd31d2fdc63329001b3dee28c08f02b8c98ac
Micah Halter <micah@mehalter.com>
2022-12-30 10:51:09 -0500

    reenable lsp progress

--- plugins/heirline.lua
+++ plugins/heirline.lua
@@ -1,6 +1,4 @@
 return function(config)
-  -- disable lsp progress
-  config[1][9] = astronvim.status.component.lsp { lsp_progress = false }
   -- disable close button in bufferline tabs
   config[3][2] =
     astronvim.status.heirline.make_buflist(astronvim.status.component.tabline_file_info { close_button = false })
