601658670e99a5e8807b275f20b67c425a42342b
Micah Halter <micah@mehalter.com>
2023-01-10 10:33:19 -0500

    update heirline

--- plugins/heirline.lua
+++ plugins/heirline.lua
@@ -1,8 +1,8 @@
 return {
   "rebelot/heirline.nvim",
   opts = function(_, opts)
-    opts[3][2] =
-      astronvim.status.heirline.make_buflist(astronvim.status.component.tabline_file_info { close_button = false })
+    opts.tabline[2] =
+    astronvim.status.heirline.make_buflist(astronvim.status.component.tabline_file_info { close_button = false })
     return opts
   end,
 }
