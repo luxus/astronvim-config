f82f39996438f779dd21e0cdd0783ace5ed7b2fd
Micah Halter <micah@mehalter.com>
2023-01-11 11:18:05 -0500

    use main neo-tree for a bit

--- plugins/init.lua
+++ plugins/init.lua
@@ -1,6 +1,8 @@
 return {
   { "goolord/alpha-nvim", enabled = false },
   { "max397574/better-escape.nvim", enabled = false },
+  -- use mainline neo-tree for testing new fix
+  { "nvim-neo-tree/neo-tree.nvim", version = false, branch = "main" },
 
   { "akinsho/toggleterm.nvim", opts = { terminal_mappings = false } },
   { "folke/which-key.nvim", opts = { window = { border = "none" } } },
