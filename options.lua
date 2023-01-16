a48ca75feb42feccf41a56f7ddc91ad757b5644f
Micah Halter <micah@mehalter.com>
2023-01-16 08:49:31 -0500

    add ufo

--- options.lua
+++ options.lua
@@ -1,9 +1,10 @@
 return {
   opt = {
     conceallevel = 2, -- enable conceal
-    foldenable = false,
-    foldexpr = "nvim_treesitter#foldexpr()", -- set Treesitter based folding
-    foldmethod = "expr",
+    foldenable = true,
+    foldlevel = 99,
+    foldlevelstart = 99,
+    foldcolumn = "1",
     linebreak = true, -- linebreak soft wrap at words
     list = true, -- show whitespace characters
     listchars = { tab = "│→", extends = "⟩", precedes = "⟨", trail = "·", nbsp = "␣" },
