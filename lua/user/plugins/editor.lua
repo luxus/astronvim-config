7cc97584305d9251d3b97cf2a6f2361745d9df94
luxus <luxuspur@gmail.com>
2023-01-21 21:04:34 +0100

    comment out the standard

--- lua/user/plugins/editor.lua
+++ lua/user/plugins/editor.lua
@@ -260,19 +260,19 @@ return {
     event = "VeryLazy",
     config = function(_, opts) require("mini.move").setup(opts) end,
     -- Module mappings. Use `''` (empty string) to disable one.
-    mappings = {
-      -- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
-      left = "<M-h>",
-      right = "<M-l>",
-      down = "<M-j>",
-      up = "<M-k>",
-
-      -- Move current line in Normal mode
-      line_left = "<M-h>",
-      line_right = "<M-l>",
-      line_down = "<M-j>",
-      line_up = "<M-k>",
-    },
+    -- mappings = {
+    --   -- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
+    --   left = "<M-h>",
+    --   right = "<M-l>",
+    --   down = "<M-j>",
+    --   up = "<M-k>",
+    --
+    --   -- Move current line in Normal mode
+    --   line_left = "<M-h>",
+    --   line_right = "<M-l>",
+    --   line_down = "<M-j>",
+    --   line_up = "<M-k>",
+    -- },
   },
   {
     "echasnovski/mini.indentscope",
