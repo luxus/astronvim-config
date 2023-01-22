90b0bf9fbbdb1cfa593d920b272108c4d997ca4a
luxus <luxuspur@gmail.com>
2023-01-22 11:26:38 +0100

    change scrollbars to standard config

--- lua/user/plugins/ui.lua
+++ lua/user/plugins/ui.lua
@@ -70,22 +70,26 @@ return {
   {
     "petertriho/nvim-scrollbar",
     event = "BufReadPost",
-    config = function()
-      local scrollbar = require "scrollbar"
-      local colors = require("tokyonight.colors").setup()
-      scrollbar.setup {
-        handle = { color = colors.bg_highlight },
-        excluded_filetypes = { "prompt", "TelescopePrompt", "noice", "notify" },
-        marks = {
-          Search = { color = colors.orange },
-          Error = { color = colors.error },
-          Warn = { color = colors.warning },
-          Info = { color = colors.info },
-          Hint = { color = colors.hint },
-          Misc = { color = colors.purple },
-        },
-      }
-    end,
+    config = true,
+    -- config = function()
+    --   local scrollbar = require "scrollbar"
+    --   local colors = require("tokyonight.colors").setup()
+    --   scrollbar.setup {
+    --     handle = { color = colors.bg_highlight },
+    --     excluded_filetypes = { "prompt", "TelescopePrompt", "noice", "notify" },
+    --     marks = {
+    --       Search = { color = colors.orange },
+    --       Error = { color = colors.error },
+    --       Warn = { color = colors.warning },
+    --       Info = { color = colors.info },
+    --       Hint = { color = colors.hint },
+    --       Misc = { color = colors.purple },
+    --     },
+    --   }
+    -- end,
+    opts = {
+      excluded_filetypes = { "prompt", "TelescopePrompt", "noice", "notify", "neo-tree" },
+    },
   },
   {
     "echasnovski/mini.animate",
