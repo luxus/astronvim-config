e103df76d77f7d50a011fd169d96417085e08942
luxus <luxuspur@gmail.com>
2023-01-26 16:36:16 +0100

    switch to mini.ai vim-matchup rainbow2 instead of mehalters textobjects

--- lua/user/plugins/editor.lua
+++ lua/user/plugins/editor.lua
@@ -258,11 +258,10 @@ return {
       { "i", mode = { "x", "o" } },
     },
     dependencies = {
-      { "JoosepAlviste/nvim-ts-context-commentstring" },
       {
         "nvim-treesitter/nvim-treesitter-textobjects",
         init = function()
-          -- no need to load the plugin, since we only need its queri es
+          -- no need to load the plugin, since we only need its queries
           require("lazy.core.loader").disable_rtp_plugin "nvim-treesitter-textobjects"
         end,
       },
