a4b1fe562d68f18e04becbfc8e34043ea622c4b9
luxus <luxuspur@gmail.com>
2023-01-28 21:04:48 +0100

    change default theme and enable autoupdate

--- lua/user/lazy.lua
+++ lua/user/lazy.lua
@@ -1,6 +1,8 @@
 -- Configure require("lazy").setup() options
 return {
   defaults = { lazy = true },
+  install = { colorscheme = { "everforest" } },
+  checker = { enabled = true }, -- automatically check for plugin updates
   performance = {
     rtp = {
       -- customize default disabled vim plugins
