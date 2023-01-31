25114afe1de17b3624465099ecd6a752e11cefc9
luxus <luxuspur@gmail.com>
2023-01-31 21:15:41 +0100

    rust updates

--- lua/user/lsp/setup_handlers.lua
+++ lua/user/lsp/setup_handlers.lua
@@ -1,5 +1,10 @@
+local function fallback_setup(plugin, server, opts)
+  local lsp_plugin_avail, _ = pcall(require, plugin)
+  if not lsp_plugin_avail then require("lspconfig")[server].setup(opts) end
+end
+
 return {
-  tsserver = function(_, opts) require("typescript").setup { server = opts } end,
-  clangd = function(_, opts) require("clangd_extensions").setup { server = opts } end,
-  rust_analyzer = function(_, opts) require("rust-tools").setup { server = opts } end,
+  tsserver = function(...) fallback_setup("typescript", ...) end,
+  clangd = function(...) fallback_setup("clangd_extensions", ...) end,
+  rust_analyzer = function(...) fallback_setup("rust-tools", ...) end,
 }
--- lua/user/plugins/extras/lang/rust.lua
+++ lua/user/plugins/extras/lang/rust.lua
@@ -1,25 +1,28 @@
 return {
   {
-    "simrat39/rust-tools.nvim",
-    event = "LspAttach",
-  },
-  {
-    "Saecki/crates.nvim",
-    opts = {
-      null_ls = {
-        enabled = true,
-        name = "crates.nvim",
-      },
+    { "simrat39/rust-tools.nvim", opts = function() return { server = astronvim.lsp.config "rust_analyzer" } end },
+    {
+      "williamboman/mason-lspconfig.nvim",
+      opts = { ensure_installed = { "rust_analyzer" } },
     },
-    init = function()
-      vim.api.nvim_create_autocmd("BufRead", {
-        group = vim.api.nvim_create_augroup("CmpSourceCargo", { clear = true }),
-        pattern = "Cargo.toml",
-        callback = function()
-          require("cmp").setup.buffer { sources = { { name = "crates" } } }
-          require "crates"
-        end,
-      })
-    end,
+    {
+      "Saecki/crates.nvim",
+      opts = {
+        null_ls = {
+          enabled = true,
+          name = "crates.nvim",
+        },
+      },
+      init = function()
+        vim.api.nvim_create_autocmd("BufRead", {
+          group = vim.api.nvim_create_augroup("CmpSourceCargo", { clear = true }),
+          pattern = "Cargo.toml",
+          callback = function()
+            require("cmp").setup.buffer { sources = { { name = "crates" } } }
+            require "crates"
+          end,
+        })
+      end,
+    },
   },
 }
