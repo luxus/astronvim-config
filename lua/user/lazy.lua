5e6d0476c871f85f816e442d8bd73eff0d920bf6
luxus <luxuspur@gmail.com>
2023-02-03 02:38:16 +0100

    using the correct way :D

--- lua/user/lazy.lua
+++ lua/user/lazy.lua
@@ -1,21 +1,55 @@
 -- Configure require("lazy").setup() options
-return {
-  defaults = { lazy = true },
-  install = { colorscheme = { "everforest" } },
-  checker = { enabled = true }, -- automatically check for plugin updates
-  performance = {
-    rtp = {
-      -- customize default disabled vim plugins
-      disabled_plugins = {
-        "nvim-treesitter-textobjects",
-        "tohtml",
-        "gzip",
-        "matchit",
-        "zipPlugin",
-        "netrwPlugin",
-        "tarPlugin",
-        "matchparen",
+return function(config)
+  vim.list_extend(config.spec, {
+    { import = "user.plugins.extras.lang.go" },
+    { import = "user.plugins.extras.lang.rust" },
+    { import = "user.plugins.extras.lang.nix" },
+    -- { import =  "user.plugins.extras.lang.typescript" },
+    -- { import =  "user.plugins.extras.lang.tex" },
+    { import = "user.plugins.extras.ui.ministarter" },
+    { import = "user.plugins.extras.ui.ccc" },
+    { import = "user.plugins.extras.ui.drop" },
+    { import = "user.plugins.extras.ui.minianimate" },
+    { import = "user.plugins.extras.ui.noice" },
+    { import = "user.plugins.extras.ui.zen-mode" },
+    -- { import =  "user.plugins.extras.ui.modicator" },
+    -- { import =  "user.plugins.extras.ui.scrollbar" },
+    { import = "user.plugins.extras.editor.minicomment" },
+    { import = "user.plugins.extras.editor.grapple+portal" },
+    { import = "user.plugins.extras.editor.copilot" },
+    -- { import =  "user.plugins.extras.editor.textobjects" },
+    { import = "user.plugins.extras.editor.miniai" },
+    -- { import =  "user.plugins.extras.editor.copilot-cmp" },
+    { import = "user.plugins.extras.editor.cutlass" },
+    { import = "user.plugins.extras.editor.neogen" },
+    -- { import =  "user.plugins.extras.editor.bqf" },
+    { import = "user.plugins.extras.tools.oil" },
+    -- { import = "user.plugins.extras.tools.iron" },
+    -- { import =  "user.plugins.extras.tools.refactoring" },
+    -- { import =  "user.plugins.extras.tools.octo" },
+    { import = "user.plugins.extras.tools.sg" },
+    { import = "user.plugins.extras.tools.spectre" },
+  })
+  return astronvim.extend_tbl(config, {
+    -- whatever else you want to override with just a simple table merge
+    defaults = { lazy = true },
+    install = { colorscheme = { "everforest" } },
+    checker = { enabled = true }, -- automatically check for plugin updates
+    performance = {
+      rtp = {
+        -- customize default disabled vim plugins
+        disabled_plugins = {
+          "nvim-treesitter-textobjects",
+          "tohtml",
+          "gzip",
+          "matchit",
+          "zipPlugin",
+          "netrwPlugin",
+          "tarPlugin",
+          "matchparen",
+        },
       },
     },
-  },
-}
+    diff = { cmd = "terminal_git" },
+  })
+end
