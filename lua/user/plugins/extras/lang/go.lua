2752d700edb5124815064e4299db488ae1953825
luxus <luxuspur@gmail.com>
2023-02-04 11:43:42 +0100

    i fucked my go config :(

--- lua/user/plugins/extras/lang/go.lua
+++ lua/user/plugins/extras/lang/go.lua
@@ -1,4 +1,37 @@
 return {
+  -- Golang support
+  {
+    "olexsmir/gopher.nvim",
+    opts = function() return { server = astronvim.lsp.config "gopls" } end,
+    ft = "go",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = true,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = { ensure_installed = { "gopls", "gomodifytags", "gofumpt", "iferr", "impl", "goimports" } },
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = { ensure_installed = { "gopls" } },
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    config = true,
+    ft = "go",
+  },
+  { "theHamsta/nvim-dap-virtual-text", ft = "go", config = true },
+  {
+    "leoluz/nvim-dap-go",
+    config = true,
+    ft = "go",
+    dependencies = {
+      { "mfussenegger/nvim-dap" },
+    },
+  },
   {
     "nvim-treesitter/nvim-treesitter",
     opts = {
@@ -6,33 +39,11 @@ return {
     },
   },
   {
-    "olexsmir/gopher.nvim",
-    config = true,
-    ft = { "go", "gomod" },
-  },
-  {
-    "rcarriga/nvim-dap-ui",
-    config = true,
-    dependencies = {
-      { "mfussenegger/nvim-dap" },
-      { "theHamsta/nvim-dap-virtual-text", config = true },
-      {
-        "leoluz/nvim-dap-go",
-        config = true,
-      },
+    "jay-babu/mason-nvim-dap.nvim",
+    opts = {
+      ensure_installed = { "delve" },
+      automatic_installation = true,
+      automatic_setup = true,
     },
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    opts = { ensure_installed = { "gopls" } },
-  },
-  -- correctly setup mason lsp / dap extensions
-  {
-    "williamboman/mason.nvim",
-    opts = { ensure_installed = {
-      "gomodifytags",
-      "impl",
-      "iferr",
-    } },
   },
 }
