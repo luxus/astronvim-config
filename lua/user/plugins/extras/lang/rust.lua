d9f353b9eee55569d463353808e26b3b00957780
luxus <luxuspur@gmail.com>
2023-02-05 21:59:09 +0100

    more typos haha

--- lua/user/plugins/extras/lang/rust.lua
+++ lua/user/plugins/extras/lang/rust.lua
@@ -1,33 +1,31 @@
 return {
   {
-    {
-      "simrat39/rust-tools.nvim",
-      ft = { "rs" },
-      opts = function() return { server = astronvim.lsp.config "rust_analyzer" } end,
-    },
-    {
-      "williamboman/mason-lspconfig.nvim",
-      opts = { ensure_installed = { "rust_analyzer", "taplo" } },
-    },
-    {
-      "Saecki/crates.nvim",
-      event = { "BufRead Cargo.toml" },
-      opts = {
-        null_ls = {
-          enabled = true,
-          name = "crates.nvim",
-        },
+    "simrat39/rust-tools.nvim",
+    ft = { "rs" },
+    opts = function() return { server = astronvim.lsp.config "rust_analyzer" } end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = { ensure_installed = { "rust_analyzer", "taplo" } },
+  },
+  {
+    "Saecki/crates.nvim",
+    event = { "BufRead Cargo.toml" },
+    opts = {
+      null_ls = {
+        enabled = true,
+        name = "crates.nvim",
       },
-      init = function()
-        vim.api.nvim_create_autocmd("BufRead", {
-          group = vim.api.nvim_create_augroup("CmpSourceCargo", { clear = true }),
-          pattern = "Cargo.toml",
-          callback = function()
-            require("cmp").setup.buffer { sources = { { name = "crates" } } }
-            require "crates"
-          end,
-        })
-      end,
     },
+    init = function()
+      vim.api.nvim_create_autocmd("BufRead", {
+        group = vim.api.nvim_create_augroup("CmpSourceCargo", { clear = true }),
+        pattern = "Cargo.toml",
+        callback = function()
+          require("cmp").setup.buffer { sources = { { name = "crates" } } }
+          require "crates"
+        end,
+      })
+    end,
   },
 }
