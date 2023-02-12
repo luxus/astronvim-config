3dc52aa7fb7e089322189438ede01fede7939754
luxus <luxuspur@gmail.com>
2023-02-12 19:41:09 +0100

    delete modicator
    this should be build with heirline, there are already functions for changing color by mode and the linenumber is in the column bar as well.

--- lua/user/lazy.lua
+++ lua/user/lazy.lua
@@ -19,7 +19,6 @@ return function(config)
     { import = "user.plugins.extras.ui.noice" },
     { import = "user.plugins.extras.ui.zen-mode" },
     -- { import = "user.plugins.extras.ui.shade" },
-    -- { import =  "user.plugins.extras.ui.modicator" },
     -- { import =  "user.plugins.extras.ui.scrollbar" },
     { import = "user.plugins.extras.editor.minicomment" },
     -- { import = "user.plugins.extras.editor.lsplines" },
