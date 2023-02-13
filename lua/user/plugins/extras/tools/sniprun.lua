02a8d254f5bd0e21bd7a3ea08688f660d2d19fad
luxus <luxuspur@gmail.com>
2023-02-13 08:22:05 +0100

    make sniprun useable

--- lua/user/plugins/extras/tools/sniprun.lua
+++ lua/user/plugins/extras/tools/sniprun.lua
@@ -1,3 +1,16 @@
 return {
-  { "michaelb/sniprun", run = "bash ./install.sh", cmd = "SnipRun" },
+  {
+    "michaelb/sniprun",
+    init = function() require("which-key").register({ ["<leader>r"] = { name = "Run" } }, { mode = "n", "v" }) end,
+    keys = {
+      { "<leader>rr", function() require("sniprun").run() end, desc = "Sniprun" },
+      { "<leader>rr", mode = { "x" }, function() require("sniprun").run "v" end, desc = "Sniprun" },
+    },
+    opts = {
+      live_mode_toggle = "enable",
+      live_display = { "VirtualText", "TerminalOk" },
+    },
+    build = "bash ./install.sh 1",
+    cmd = "SnipRun",
+  },
 }
