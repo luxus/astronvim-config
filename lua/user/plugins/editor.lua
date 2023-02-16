0203f17a16d80deaed0b7cc46590bf74b4bd97b9
luxus <luxuspur@gmail.com>
2023-02-16 22:46:23 +0100

    updates to trouble

--- lua/user/plugins/editor.lua
+++ lua/user/plugins/editor.lua
@@ -151,7 +151,15 @@ return {
   -- make todo, fix etc stand out
   {
     "folke/todo-comments.nvim",
+    cmd = { "TodoTrouble", "TodoTelescope" },
     config = true,
     init = function() table.insert(astronvim.file_plugins, "todo-comments.nvim") end,
+    keys = {
+      { "]t", function() require("todo-comments").jump_next() end, desc = "Next todo comment" },
+      { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous todo comment" },
+      { "<leader>xt", "<cmd>TodoTrouble<cr>", desc = "Todo (Trouble)" },
+      { "<leader>xT", "<cmd>TodoTrouble keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme (Trouble)" },
+      { "<leader>st", "<cmd>TodoTelescope<cr>", desc = "Todo" },
+    },
   },
 }
