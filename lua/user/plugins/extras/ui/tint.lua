64c1d126cd210986480eaf5e964dd46e967edf8d
luxus <luxuspur@gmail.com>
2023-02-18 19:59:59 +0100

    load tint on file

--- lua/user/plugins/extras/ui/tint.lua
+++ lua/user/plugins/extras/ui/tint.lua
@@ -1,7 +1,7 @@
 return {
   {
     "levouh/tint.nvim",
-    event = "VeryLazy",
+    init = function() table.insert(astronvim.file_plugins, "tint.nvim") end,
     opts = {
       tint = -45, -- Darken colors, use a positive value to brighten
       saturation = 0.6, -- Saturation to preserve
