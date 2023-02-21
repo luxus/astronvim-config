a57f722cea3ede5b679831df54b3f0583fa0e8d0
luxus <luxuspur@gmail.com>
2023-02-21 03:30:59 +0100

    adding codelldb

--- lua/user/plugins/extras/lang/rust.lua
+++ lua/user/plugins/extras/lang/rust.lua
@@ -9,6 +9,14 @@ return {
     "simrat39/rust-tools.nvim",
     ft = { "rust" },
     opts = function() return { server = require("core.utils.lsp").config "rust_analyzer" } end,
+    dependencies = {
+      {
+        "jay-babu/mason-nvim-dap.nvim",
+        opts = function(_, opts)
+          if type(opts.ensure_installed) == "table" then vim.list_extend(opts.ensure_installed, { "codelldb" }) end
+        end,
+      },
+    },
   },
   {
     "williamboman/mason-lspconfig.nvim",
