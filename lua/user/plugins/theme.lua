b31d38e2b6dbe8ca858c5bf60f40a630de7d0de0
Uzair Aftab <uzaaft@outlook.com>
2023-03-12 16:40:43 +0100

    feat: some time tweaks

--- lua/user/plugins/theme.lua
+++ lua/user/plugins/theme.lua
@@ -1,15 +1,18 @@
 return {
-  "luxus/colorful-times-nvim",
-  lazy = false,
-  opts = {
-    default = {
-      theme = "everforest", -- the default theme to use if no timeframes match
-      bg = "dark", -- the default background to use
-    },
-    timeframes = { -- the timeframes to use
-      { theme = "rose-pine", start = "21:58", stop = "03:07" },
-      { theme = "everforest", start = "06:10", stop = "16:44", bg = "light" },
-      { theme = "tokyonight", start = "16:45", stop = "21:46", bg = "dark" },
+  {
+    "luxus/colorful-times-nvim",
+    lazy = false,
+    opts = {
+      default = {
+        theme = "everforest", -- the default theme to use if no timeframes match
+        bg = "dark", -- the default background to use
+      },
+      timeframes = { -- the timeframes to use
+        { theme = "rose-pine", start = "21:58", stop = "03:07" },
+        { theme = "everforest", start = "06:10", stop = "15:00", bg = "light" },
+        { theme = "tokyonight", start = "16:45", stop = "21:46", bg = "dark" },
+      },
     },
   },
+  { "AstroNvim/astrotheme", branch = "nightly" },
 }
