210350051f33ced842bc2ff7380c1d372707c22a
luxus <luxuspur@gmail.com>
2023-04-02 15:35:16 +0200

    highlight groups stolen from mehalter for git blame

--- lua/user/highlights/init.lua
+++ lua/user/highlights/init.lua
@@ -2,6 +2,7 @@ return function()
   local get_hlgroup = require("astronvim.utils").get_hlgroup
   local normal = get_hlgroup "Normal"
   local fg, bg = normal.fg, normal.bg
+  local nontext = get_hlgroup "NonText"
   local bg_alt = get_hlgroup("Visual").bg
   local green = get_hlgroup("String").fg
   local red = get_hlgroup("Error").fg
@@ -24,5 +25,6 @@ return function()
     NeoTreeTabSeparatorActive = { fg = fg, bg = bg_alt },
     NeoTreeTabSeparatorInactive = { fg = bg_alt, bg = bg },
     NeoTreeTabActive = { fg = fg, bg = bg_alt },
+    GitSignsCurrentLineBlame = { fg = nontext.fg, italic = true },
   }
 end
