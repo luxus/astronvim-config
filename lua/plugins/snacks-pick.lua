771d15b63821c2dc12898db2f9a7ca39e843adb7
luxus <luxuspur@gmail.com>
2025-01-25 00:43:37 +0100

    more

--- lua/plugins/snacks-pick.lua
+++ lua/plugins/snacks-pick.lua
@@ -1,21 +1,17 @@
 return {
   "folke/snacks.nvim",
-  init = function() require("astrocore").load_plugin_with_func("snacks.nvim", vim.ui, "select") end,
-  opts = { picker = { ui_select = true, formatters = { file = { filename_first = true } } } },
+  priority = 1000,
+  lazy = false,
+  dependencies = { "nvim-treesitter/nvim-treesitter" },
+  opts = { picker = { ui_select = true } },
   specs = {
     {
       "AstroNvim/astrocore",
       opts = function(_, opts)
         local maps = opts.mappings
-        -- local astro = require "astrocore"
-        -- local is_available = astro.is_available
         maps.n["<Leader>f"] = vim.tbl_get(opts, "_map_sections", "f")
         if vim.fn.executable "git" == 1 then
           maps.n["<Leader>g"] = vim.tbl_get(opts, "_map_sections", "g")
-          -- maps.n["<Leader>gb"] = {
-          --   function() require("telescope.builtin").git_branches { use_file_path = true } end,
-          --   desc = "Git branches",
-          -- }
           maps.n["<Leader>gc"] = {
             function() require("snacks").picker.git_log() end,
             desc = "Git commits (repository)",
@@ -55,12 +51,11 @@ return {
         maps.n["<Leader>fh"] = { function() require("snacks").picker.help() end, desc = "Find help" }
         maps.n["<Leader>fk"] = { function() require("snacks").picker.keymaps() end, desc = "Find keymaps" }
         maps.n["<Leader>fm"] = { function() require("snacks").picker.man() end, desc = "Find man" }
-        -- if is_available "nvim-notify" then
-        --   maps.n["<Leader>fn"] =
-        --     { function() require("telescope").extensions.notify.notify() end, desc = "Find notifications" }
-        -- end
         maps.n["<Leader>fo"] = { function() require("snacks").picker.recent() end, desc = "Find old files" }
+        maps.n["<Leader>fO"] =
+          { function() require("snacks").picker.recent { cwd = true } end, desc = "Find old files (cwd)" }
         maps.n["<Leader>fr"] = { function() require("snacks").picker.registers() end, desc = "Find registers" }
+        maps.n["<Leader>fs"] = { function() require("snacks").picker.smart() end, desc = "Find buffers/recent/files" }
         maps.n["<Leader>ft"] = { function() require("snacks").picker.colorschemes() end, desc = "Find themes" }
         if vim.fn.executable "rg" == 1 then
           maps.n["<Leader>fw"] = { function() require("snacks").picker.grep() end, desc = "Find words" }
@@ -70,16 +65,7 @@ return {
           }
         end
         maps.n["<Leader>lD"] = { function() require("snacks").picker.diagnostics() end, desc = "Search diagnostics" }
-        maps.n["<Leader>ls"] = {
-          function()
-            -- if is_available "aerial.nvim" then
-            --   require("telescope").extensions.aerial.aerial()
-            -- else
-            require("snacks").picker.lsp_symbols()
-            -- end
-          end,
-          desc = "Search symbols",
-        }
+        maps.n["<Leader>ls"] = { function() require("snacks").picker.lsp_symbols() end, desc = "Search symbols" }
       end,
     },
     {
@@ -92,12 +78,15 @@ return {
           opts = {
             mappings = {
               n = {
-                ["<Leader>fT"] = function()
-                  if not package.loaded["todo-comments"] then -- make sure to load todo-comments
-                    require("lazy").load { plugins = { "todo-comments.nvim" } }
-                  end
-                  require("snacks").picker.todo_comments()
-                end,
+                ["<Leader>fT"] = {
+                  function()
+                    if not package.loaded["todo-comments"] then -- make sure to load todo-comments
+                      require("lazy").load { plugins = { "todo-comments.nvim" } }
+                    end
+                    require("snacks").picker.todo_comments()
+                  end,
+                  desc = "Todo Comments",
+                },
               },
             },
           },
--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -4,4 +4,5 @@ return {
   { "AstroNvim/astrocore", version = false, branch = "v2" },
   { "AstroNvim/astrolsp", version = false, branch = "v3" },
   { "AstroNvim/astroui", version = false, branch = "v3" },
+  { "markview.nvim", opts = { filetypes = { "markdown", "markdown.mdx", "quarto", "typst" } } },
 }
